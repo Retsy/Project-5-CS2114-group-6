23a5c0cd9897f3fdc9f479fc66b8cc714b92d0b1
Retsy <hotlhk@hotmail.com>
2016-08-09 14:38:21 -0400

    Add files via upload

---
+++ Position.java
@@ -0,0 +1,19 @@
+package project5;
+
+/**
+ * The enumerator Position is to determine the Bar position.
+ * 
+ * @author  Haokun Liu (haokun)
+ * @version 2016.08.09
+ */
+
+public enum Position 
+{
+    NorthEast, SouthEast, NorthWest, SouthWest,
+    North, South, West, East, Middle;
+    
+    Position()
+    {
+        //This is empty.
+    }
+}
